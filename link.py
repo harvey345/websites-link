11d2ddb8c6518866b8f15dcde47e66830496c5ce
Harvey Hsieh <s1040080@korrnell.hcc.edu.tw>
2020-02-14 18:17:13 +0800

    add more links

--- link.py
+++ link.py
@@ -1,30 +1,45 @@
 print("These are the links to the websites.")
-print("\n1:Google   2:Facebook   3:Instagram   \n\n4:Youtube   5:G-mail   6:Tinkercad   \n\n7:Github   8:Scratch (Blocks for kid)   \n\n9:Google-Drive   10:Microbit   \n\n11:Amazon   12:Google Map")
+print("\n1:Google   2:Facebook   3:Instagram   4:Youtube   \n\n5:G-mail   6:Tinkercad   7:Github   8:Scratch (Blocks for kid)   \n\n9:Google-Drive   10:Microbit   11:Amazon   12:Google Map   \n\n13:Mcgraw Hill   14:Mobile 01   15:Google Play   16:Bitbucket   ")
+print("\n17:Mobile 02   18:Code-War   19:Wikipedia   ")
 ll=int(input("\nEnter the number of the website you want.(Only One!)\n"))
 if ll==1:
-    print("Google:\nhttps://www.google.com/")
+    print("\nGoogle:\nhttps://www.google.com/")
 elif ll==2:
-    print("Facebook:\nhttps://www.facebook.com/")
+    print("\nFacebook:\nhttps://www.facebook.com/")
 elif ll==3:
-    print("Instagram:\nhttps://www.instagram.com/?hl=zh-tw")
+    print("\nInstagram:\nhttps://www.instagram.com/?hl=zh-tw")
 elif ll==4:
-    print("Youtube:\nhttps://www.youtube.com/")
+    print("\nYoutube:\nhttps://www.youtube.com/")
 elif ll==5:
-    print("G-mail:\nhttps://www.google.com/gmail/about/")
+    print("\nG-mail:\nhttps://www.google.com/gmail/about/")
 elif ll==6:
-    print("tinkercad:\nhttps://www.tinkercad.com/")
+    print("\ntinkercad:\nhttps://www.tinkercad.com/")
 elif ll==7:
-    print("Github:\nhttps://circleci.com/integrations/github/?utm_source=gnb&utm_medium=SEM&utm_campaign=SEM-gnb-Github-Eng-ni&utm_content=SEM-gnb-Github-Eng-ni-Github&gclid=EAIaIQobChMI0_i8nrS55wIVCkHTCh3r8gBOEAAYASAAEgLzxfD_BwE")
+    print("\nGithub:\nhttps://circleci.com/integrations/github/?utm_source=gnb&utm_medium=SEM&utm_campaign=SEM-gnb-Github-Eng-ni&utm_content=SEM-gnb-Github-Eng-ni-Github&gclid=EAIaIQobChMI0_i8nrS55wIVCkHTCh3r8gBOEAAYASAAEgLzxfD_BwE")
 elif ll==8:
-    print("Scratch (Blocks for kid):\nhttps://scratch.mit.edu/")
+    print("\nScratch (Blocks for kid):\nhttps://scratch.mit.edu/")
 elif ll==9:
-    print("Google-Drive:\nhttps://www.google.com/intl/zh-HK_ALL/drive/using-drive/")
+    print("\nGoogle-Drive:\nhttps://www.google.com/intl/zh-HK_ALL/drive/using-drive/")
 elif ll==10:
-    print("Microbit:\nhttps://microbit.org/hk/code/")
+    print("\nMicrobit:\nhttps://microbit.org/hk/code/")
 elif ll==11:
-    print("Amazon:\nhttps://www.amazon.com/s?k=bose+solo5&ref=nb_sb_noss_1")
+    print("\nAmazon:\nhttps://www.amazon.com/s?k=bose+solo5&ref=nb_sb_noss_1")
 elif ll==12:
-    print("Google Map:\nhttps://www.google.com.tw/maps?hl=zh-TW&tab=rl1")
+    print("\nGoogle Map:\nhttps://www.google.com.tw/maps?hl=zh-TW&tab=rl1")
+elif ll==13:
+    print("\nhttps://www.mheducation.com/")
+elif ll==14:
+    print("\nMobile 01:https://www.mobile01.com/")
+elif ll==15:
+    print("\nGoogle Play:\nhttps://play.google.com/store/apps/details?id=com.google.android.apps.docs&hl=zh_TW")
+elif ll==16:
+    print("\nBitbucket:\nhttps://bitbucket.org/")
+elif ll==17:
+    print("\nMobile 02:\nhttps://www.mobile02.com.tw/")
+elif ll==18:
+    print("\nCode-War:\nhttps://www.codewars.com/")
+elif ll==19:
+    print("\nWikipedia:\nhttps://www.wikipedia.org/")
 else:
-    print("We haven't set the website you want.")
+    print("\nWe haven't set the website you want.")
 print()
